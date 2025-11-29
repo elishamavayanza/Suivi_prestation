c1654c05f4b4a09d4ef0358b49721ec72f3cb2e5
Elishama Vayanza <vayanzaelishama@gmail.com>
2025-11-29 09:02:05 +0200

    interface accademique

--- academique/academique_prestation.php
+++ academique/academique_prestation.php
@@ -14,7 +14,7 @@ include '../config/connexion.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fiches de Prestation - Académique</title>
-    <link rel="stylesheet" href="academic_styles.css">
+    <link rel="stylesheet" href="academic_admin_styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
--- academique/horaire.php
+++ academique/horaire.php
@@ -14,7 +14,7 @@ include '../config/connexion.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des Horaires - Service Académique</title>
-    <link rel="stylesheet" href="academic_styles.css">
+    <link rel="stylesheet" href="academic_admin_styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
